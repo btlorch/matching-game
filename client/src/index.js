cd62745a2364d3d77d4acc969992bcbe1bca9816
Benedikt Lorch <benedikt.t.lorch@dartmouth.edu>
2018-04-15 04:24:56 -0400

    Add game logic. Remove matched items. Display feedback whether cards match or did not match

--- client/src/index.js
+++ client/src/index.js
@@ -4,20 +4,31 @@ import './index.css';
 
 
 function Card(props) {
+    // Shift to center
     let style = {
-        top: props.y,
-        left: props.x,
+        top: props.y - 100,
+        left: props.x - 100,
         transform: `rotate( ${props.rotation}deg )`,
     };
 
+    let faceClass = "face";
+    if (props.open) {
+        faceClass += " open";
+    }
+    if (props.match) {
+        faceClass += " match";
+    }
+    if (props.mismatch) {
+        faceClass += " mismatch";
+    }
+    if (props.hidden) {
+        faceClass += " hidden";
+    }
+
     return (
-        <div
-            className="card"
-            style={style}
-            data-x={props.x}
-            data-y={props.y}
-            data-rotation={props.rotation}>
-            <div className={props.open ? "face flipped" : "face"} onClick={props.onClick}>
+        <div className="card"
+             style={style}>
+            <div className={faceClass} onClick={props.onClick}>
                 <div className="front">
                     <img src="http://via.placeholder.com/200x200" alt="Bla"/>
                 </div>
@@ -33,29 +44,108 @@ function Card(props) {
 class CardDeck extends React.Component {
     constructor(props) {
         super(props);
+        this.cards = createCards(12);
         this.state = {
-            cards: createCards(12)
+            cardProperties: this.cards.map(() => {
+                return {
+                    open: false,
+                    match: false,
+                    mismatch: false,
+                    hidden: false,
+                }
+            })
         };
     }
 
     handleClick(i) {
-        const cards = this.state.cards.slice();
+        // Ignore clicks on already open cards
+        if (this.state.cardProperties[i].open) {
+            return false;
+        }
+
+        let openCardsIndices = this.state.cardProperties.reduce((a, e, i) => (e.open) ? a.concat(i) : a, []);
+        // Also ignore clicks as long as there are two cards open
+        if (openCardsIndices.length >= 2) {
+            return;
+        }
+
         // Flip open
-        cards[i].open = !cards[i].open;
+        let cardProperties = this.state.cardProperties.slice();
+        cardProperties[i].open = true;
         this.setState({
-            cards: cards
+            cardProperties: cardProperties
         });
+
+        // If two cards are open and they match, highlight them up for a second
+        openCardsIndices = cardProperties.reduce((a, e, i) => (e.open) ? a.concat(i) : a, []);
+        if (openCardsIndices.length < 2) {
+            return;
+        }
+        else if (openCardsIndices.length > 2) {
+            // This case should not happen, because we have checked for two open cards before flipping another card open already
+            alert("More than two cards open");
+        }
+        else {
+            // Now there are two cards open
+            cardProperties = cardProperties.slice();
+            // Do they match
+            let matches = (this.cards[openCardsIndices[0]].matchKey === this.cards[openCardsIndices[1]].matchKey);
+            if (matches) {
+                // Highlight cards in green
+                cardProperties[openCardsIndices[0]].match = true;
+                cardProperties[openCardsIndices[1]].match = true;
+            }
+            else {
+                // Highlight cards in red
+                cardProperties[openCardsIndices[0]].mismatch = true;
+                cardProperties[openCardsIndices[1]].mismatch = true;
+            }
+
+            this.setState({
+                cardProperties: cardProperties
+            });
+            let that = this;
+
+            // Set timer to do something with those cards
+            setTimeout(function() {
+                cardProperties = cardProperties.slice();
+                cardProperties[openCardsIndices[0]].open = false;
+                cardProperties[openCardsIndices[1]].open = false;
+                console.log(matches);
+
+                if (matches) {
+                    // Remove cards
+                    cardProperties[openCardsIndices[0]].hidden = true;
+                    cardProperties[openCardsIndices[1]].hidden = true;
+                    cardProperties[openCardsIndices[0]].match = false;
+                    cardProperties[openCardsIndices[1]].match = false;
+                }
+                else {
+                    // Remove highlight and flip closed
+                    cardProperties[openCardsIndices[0]].mismatch = false;
+                    cardProperties[openCardsIndices[1]].mismatch = false;
+                }
+
+                that.setState({
+                    cardProperties: cardProperties
+                });
+            }, 1000);
+        }
+
     }
 
     renderCard(i) {
-        let card = this.state.cards[i];
+        let card = this.cards[i];
         return (
             <Card
                 key={i}
                 x={card.x * this.props.gameWindowWidth}
                 y={card.y * this.props.gameWindowHeight}
                 rotation={card.rotation}
-                open={card.open}
+                open={this.state.cardProperties[i].open}
+                match={this.state.cardProperties[i].match}
+                mismatch={this.state.cardProperties[i].mismatch}
+                hidden={this.state.cardProperties[i].hidden}
                 onClick={() => this.handleClick(i)}
             />
         );
@@ -65,7 +155,7 @@ class CardDeck extends React.Component {
         return (
             <div className="card-deck">
             {
-                this.state.cards.map((card, i) => this.renderCard(i))
+                this.cards.map((card, i) => this.renderCard(i))
             }
             </div>
         );
@@ -125,14 +215,20 @@ function createCards(numCards) {
     let cards = Array(numCards);
     let rows = Math.floor(Math.sqrt(numCards));
     let cols = Math.ceil(numCards * 1.0 / rows);
+
+    // The largest row values is going to be (rows - 1) / rows, thus we can move everything by 1 / (rows / 2)
+    let rowOffset = 1.0 / (rows * 2.0);
+    let colOffset = 1.0 / (cols * 2.0);
+
+    // x and y are going to be the center coordinates
     for (let i = 0; i < numCards; i++) {
         let row = Math.floor(i / cols);
         let col = i % cols;
         cards[i] = {
-            x: col * 1.0 / cols,
-            y: row * 1.0 / rows,
+            x: col * 1.0 / cols + colOffset,
+            y: row * 1.0 / rows + rowOffset,
             rotation: Math.round(Math.random() * 20) - 10,
-            open: false,
+            matchKey: Math.floor(i / 2),
         };
     }
     return cards;
